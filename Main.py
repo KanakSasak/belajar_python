3c7a8ac28423de64841e24a3a59c46da40330296
Lalu Raynaldi Pratama Putra <laluraynaldi@gmail.com>
2018-01-14 19:57:22 +0700

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,5 @@
+import modul_1
+
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+
+modul_1.func(dict)
---
+++ modul_1.py
@@ -0,0 +1,12 @@
+def func(param1):
+    dict = param1
+    param3 = "show"
+    # dict[param1] = param2
+
+    if param3 == "show":
+        for i in dict:
+            print("data", i + " = ", dict[i])
+    else:
+        print("lanjut")
+
+    return
